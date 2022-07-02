070f58e6b937f4cbbbe70fa9ce9bf27475e65d13
minhdaidevweb <minhdaidevweb@gmail.com>
2022-07-03 00:10:11 +0700

    fix replaceDeck and updateDeck

--- src/api/services/deck.service.js
+++ src/api/services/deck.service.js
@@ -25,44 +25,46 @@ const DeckService = {
     const { id } = params
     const deck = await Deck.findById(id)
     if (deck) {
-      const owner = await User.findById(deck.owner)
-      owner.decks.pull(deck)
-      await owner.save()
-      const newDeck = await Deck.findByIdAndUpdate(id, body, { new: true })
-      const newOwner = await User.findById(body.owner)
-      if (newOwner) {
-        await newDeck.save()
-        newOwner.decks.push(newDeck._id)
-        await newOwner.save()
+      const ownerId = deck.owner.toString()
+      const newOwnerId = body.owner.toString()
+      if (ownerId === newOwnerId) {
+        const newDeck = await Deck.findByIdAndUpdate(id, body, { new: true })
         return newDeck
-      } else return `Can't not find owner.`
+      } else {
+        const owner = await User.findById(deck.owner)
+        owner.decks.pull(deck)
+        await owner.save()
+        const newDeck = await Deck.findByIdAndUpdate(id, body, { new: true })
+        const newOwner = await User.findById(body.owner)
+        if (newOwner) {
+          newOwner.decks.push(newDeck._id)
+          await newOwner.save()
+          return newDeck
+        } else return `Can't not find owner.`
+      }
     } else return `Can't not find deck.`
   },
   updateDeck: async (params, body) => {
     const { id } = params
     const deck = await Deck.findById(id)
     if (deck) {
-      const owner = await User.findById(deck.owner)
-      owner.decks.pull(deck)
-      await owner.save()
-      const newDeck = await Deck.findByIdAndUpdate(id, body, { new: true })
-      const newOwner = await User.findById(body.owner)
-      if (newOwner) {
-        await newDeck.save()
-        newOwner.decks.push(newDeck._id)
-        await newOwner.save()
+      const ownerId = deck.owner.toString()
+      const newOwnerId = body.owner.toString()
+      if (ownerId === newOwnerId) {
+        const newDeck = await Deck.findByIdAndUpdate(id, body, { new: true })
         return newDeck
-      } else return `Can't not find owner.`
-    } else return 'Can not find deck.'
-  },
-  deleteDeck: async (params) => {
-    const { id } = params
-    const deck = await Deck.findById(id)
-    if (deck) {
-      const owner = await User.findById(deck.owner)
-      owner.decks.pull(deck)
-      await owner.save()
-      return await Deck.findByIdAndDelete(id)
+      } else {
+        const owner = await User.findById(deck.owner)
+        owner.decks.pull(deck)
+        await owner.save()
+        const newDeck = await Deck.findByIdAndUpdate(id, body, { new: true })
+        const newOwner = await User.findById(body.owner)
+        if (newOwner) {
+          newOwner.decks.push(newDeck._id)
+          await newOwner.save()
+          return newDeck
+        } else return `Can't not find owner.`
+      }
     } else return `Can't not find deck.`
   },
 }
